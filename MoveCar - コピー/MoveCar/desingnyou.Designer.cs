a956093a5425e062b7ccaf76579a39da62c9cc20
YokohamaNanryo_PC_Club <nanryoupc2017@yahoo.co.jp>
2017-06-29 17:18:03 +0900

    新規プッシュ

---
+++ MoveCar - コピー/MoveCar/desingnyou.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace MoveCar
+{
+	partial class desingnyou
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(desingnyou));
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(264, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(320, 128);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "スコア\r\nTime : xx\'xx\r\nPoint : XXXpt";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Location = new System.Drawing.Point(0, 48);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(176, 72);
+			this.panel1.TabIndex = 1;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(72, 48);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(32, 24);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(56, 16);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(56, 32);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(312, 152);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// desingnyou
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(585, 230);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.label1);
+			this.Location = new System.Drawing.Point(0, 90);
+			this.Name = "desingnyou";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "desingnyou";
+			this.panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ MoveCar - コピー/MoveCar/desingnyou.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MoveCar
+{
+	/// <summary>
+	/// Description of desingnyou.
+	/// </summary>
+	public partial class desingnyou : Form
+	{
+		public desingnyou()
+		{
+			
+		}
+	}
+}
